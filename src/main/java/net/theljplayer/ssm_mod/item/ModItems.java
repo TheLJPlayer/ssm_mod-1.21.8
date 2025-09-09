7b1e80d439eb00275d8743de576c9bcef8c3d6cc
Samue <samueldajackson07@gmail.com>
2025-09-08 20:20:38 -0500

    Error With Item Tags

--- src/main/java/net/theljplayer/ssm_mod/item/ModItems.java
+++ src/main/java/net/theljplayer/ssm_mod/item/ModItems.java
@@ -3,16 +3,29 @@ package net.theljplayer.ssm_mod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.theljplayer.ssm_mod.SandstoneItemsAndProgression;
+import net.theljplayer.ssm_mod.util.ModTags;
 
 import java.util.function.Function;
 
 public class ModItems {
+
+    //Materials
+    public static final ToolMaterial STEEL_TOOL_MATERIAL = new ToolMaterial(
+            ModTags.Blocks.INCORRECT_FOR_STEEL_TOOL,
+            1021,
+            7,
+            3,
+            12,
+            ModTags.Items.STEEL_TOOL_MATERIAL
+    );
 
     //Items
 
@@ -22,6 +35,11 @@ public class ModItems {
     public static final Item STEEL_NUGGET = itemRegister("steel_nugget", Item::new, new Item.Settings());
     public static final Item RAW_STEEL_SCRAP = itemRegister("raw_steel_scrap", Item::new, new Item.Settings());
     public static final Item RAW_STEEL = itemRegister("raw_steel", Item::new, new Item.Settings());
+    public static final Item STEEL_SWORD = itemRegister("steel_sword", Item::new,new Item.Settings().sword(STEEL_TOOL_MATERIAL,4f, 1.4f));
+    public static final Item STEEL_AXE = itemRegister("steel_axe", Item::new,new Item.Settings().axe(STEEL_TOOL_MATERIAL, 6f,0.8f));
+    public static final Item STEEL_PICKAXE = itemRegister("steel_pickaxe", Item::new,new Item.Settings().pickaxe(STEEL_TOOL_MATERIAL,2f,1f));
+    public static final Item STEEL_SHOVEL = itemRegister("steel_shovel", Item::new,new Item.Settings().shovel(STEEL_TOOL_MATERIAL, 2f, 1f));
+    public static final Item STEEL_HOE = itemRegister("steel_hoe", Item::new,new Item.Settings().shovel(STEEL_TOOL_MATERIAL, 3f, 2f));
 
     //Item Registries
     private static Item itemRegister(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
@@ -40,7 +58,16 @@ public class ModItems {
             fabricItemGroupEntries.add(STEEL_NUGGET);
             fabricItemGroupEntries.add(RAW_STEEL_SCRAP);
             fabricItemGroupEntries.add(RAW_STEEL);
-
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(STEEL_AXE);
+            fabricItemGroupEntries.add(STEEL_PICKAXE);
+            fabricItemGroupEntries.add(STEEL_SHOVEL);
+            fabricItemGroupEntries.add(STEEL_HOE);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(STEEL_SWORD);
+            fabricItemGroupEntries.add(STEEL_AXE);
         });
     }
 }
